a6ef1339633c7d2e75eed8ff569cf0228a5beadc
Kyle Robbertze <kyle@paddatrapper.com>
2020-12-23 21:57:59 +0200

    allow traffic from all hosts

--- api/libretimeapi/settings.py
+++ api/libretimeapi/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = get_random_string(CONFIG.get('general', 'api_key', fallback=''))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('LIBRETIME_DEBUG', False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
